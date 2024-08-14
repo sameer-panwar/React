1199af874484b887af1b75f4de2d67d313005d1a
sameer-panwar <sameerpanwar9720@gmail.com>
2024-08-14 12:05:36 +0530

    initial commit

---
+++ 01react/src/App.js
@@ -0,0 +1,13 @@
+import Display from "./sample";
+
+function App() {
+ return(
+    <>
+    <h1>Hello Programmers</h1>
+  <Display />
+
+  </>
+ )
+}
+
+export default App;
---
+++ 01react/src/sample.js
@@ -0,0 +1,7 @@
+function Display(){
+    return(
+        <h3>How are you ??</h3>
+    )
+}
+
+export default Display
