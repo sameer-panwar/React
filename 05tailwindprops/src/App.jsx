18b41a3326ca9b8b10222f94a90ff258b540c29a
sameer-panwar <sameerpanwar9720@gmail.com>
2024-08-17 21:07:05 +0530

    added margin to the heading

--- 05tailwindprops/src/App.jsx
+++ 05tailwindprops/src/App.jsx
@@ -8,9 +8,12 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-green-500 text-white p-3 rounded-xl'>Here we go with tailwind</h1>
-      <Card username="Sameer" btnText="Click here"/>
-      <Card username="Aryan" btnText="visit me"/>
+      <h1 className='bg-green-500 text-white p-3 rounded-xl mb-8'>Here we go with tailwind</h1>
+      <div className='flex gap-8'>
+        <Card username="Sameer" btnText="Click here"/>
+        <Card username="Aryan" btnText="visit me"/>
+      </div>
+      
     </>
   )
 }
