60c8bca021d36e717ca59cf5a6b3fef03c72b718
sameer-panwar <sameerpanwar9720@gmail.com>
2024-08-19 13:45:30 +0530

    Initial commit

--- 06bgChanger/src/App.jsx
+++ 06bgChanger/src/App.jsx
@@ -9,10 +9,10 @@ function App() {
         <div className="w-full h-screen duration-200"
         style={{backgroundColor: color}}
         >
-          
+
           <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
             <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-              <button className="outline-none px-4 rounded-3xl bg-red-500 font-bold text-white hover:opacity"
+              <button className="outline-none px-4 rounded-3xl bg-red-800 font-bold text-white hover:bg-red-400"
               onClick={()=> {setColor("red")}}
               >Red</button>
               <button className="outline-none px-4 rounded-3xl font-bold  text-white"
